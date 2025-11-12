e093b9365381001c1baa57d6bcc13999e65fe642
Lucas Garron <code@garron.net>
2025-11-11 16:41:13 -0800

    Fix import from `@cubing/dev-config/esbuild/es2022`.

--- script/build.ts
+++ script/build.ts
@@ -1,8 +1,8 @@
-import { es2022 } from "@cubing/dev-config/esbuild/es2022";
+import { es2022Lib } from "@cubing/dev-config/esbuild/es2022";
 import { build } from "esbuild";
 
 await build({
-  ...es2022,
+  ...es2022Lib(),
   entryPoints: ["src/main.ts"],
   outdir: "./dist/bin/@cubing/deploy",
 });
